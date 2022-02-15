cc5931b7a7ca23843638942610516fe17c76e544
Alan <cloojure@gmail.com>
2022-02-15 14:52:46 -0800

    misc

--- project.clj
+++ project.clj
@@ -4,21 +4,21 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-                 [com.h2database/h2 "1.4.200"]
+                 [com.h2database/h2 "1.4.200"] ; #todo cannot upgrade yet or crash!
                  [hikari-cp "2.13.0"]
                  ; #todo try porsas
                  ; [org.clojure/java.jdbc "0.7.11"]
-                 [org.clojure/clojure "1.10.1"]
-                 [org.clojure/test.check "1.1.0"]
-                 [org.postgresql/postgresql "42.2.18"] ; https://mvnrepository.com/artifact/org.postgresql/postgresql
-                 [prismatic/schema "1.1.12"]
-                 [seancorfield/next.jdbc "1.1.613"]
-                 [tupelo "21.01.05"]
+                 [org.clojure/clojure "1.10.3"]
+                 [org.clojure/test.check "1.1.1"]
+                 ; [org.postgresql/postgresql "42.2.18"] ; https://mvnrepository.com/artifact/org.postgresql/postgresql
+                 [prismatic/schema "1.2.0"]
+                 [seancorfield/next.jdbc "1.2.659"]
+                 [tupelo "22.02.09"]
                  ]
   :profiles {:dev     {:dependencies []
-                       :plugins      [[com.jakemccrary/lein-test-refresh "0.24.1"]
-                                      [lein-ancient "0.6.15"]
-                                      [lein-codox "0.10.7"]]}
+                       :plugins      [[com.jakemccrary/lein-test-refresh "0.25.0"]
+                                      [lein-ancient "0.7.0"]
+                                      ]}
              :uberjar {:aot :all}}
 
   :global-vars {*warn-on-reflection* false}
